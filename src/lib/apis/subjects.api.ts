2e8512020962a72ce88e25cac8528b604c28da5d
Seif Eldin <seifeldinn4@gmail.com>
2025-09-12 05:30:12 +0300

    add NEXT_PUBLIC before any variable in env 14

--- src/lib/apis/subjects.api.ts
+++ src/lib/apis/subjects.api.ts
@@ -1,8 +1,19 @@
+// export async function getSubjects(page = 1, limit = 6) {
+//   const response = await fetch(`/api/subjects?page=${page}&limit=${limit}`);
+
+//   const payload: ApiResponse<PaginatedResponse<{ subjects: Subject[] }>> =
+//     await response.json();
+
+//   if ("code" in payload) {
+//     throw new Error(payload.message);
+//   }
+
+//   return payload;
+// }
+
 export async function getSubjects(page = 1, limit = 6) {
   const response = await fetch(`/api/subjects?page=${page}&limit=${limit}`);
-
-  const payload: ApiResponse<PaginatedResponse<{ subjects: Subject[] }>> =
-    await response.json();
+  const payload = await response.json();
 
   if ("code" in payload) {
     throw new Error(payload.message);
